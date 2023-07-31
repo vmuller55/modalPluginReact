f5dfe220f7736d73ccc3adb214b688825c9611e8
Vincent Muller <v.muller55000@gmail.com>
2023-07-31 18:42:36 +0200

    test

--- react_modal/src/Modal.jsx
+++ react_modal/src/Modal.jsx
@@ -2,26 +2,11 @@ import React from 'react';
 import './modal.css'
 
 function Modal(props) {
-
-    // useState/Effect to trigger modal display
-    const [modalDisplay, setmodalDisplay] = React.useState(props.display);
-    const onButtonClick = () => { 
-        setmodalDisplay("none") 
-    }
-    React.useEffect(() => {
-        var showModal = (props.display ? 'flex' : 'none');
-        setmodalDisplay(showModal);
-    }, [props.display])
-
-    return  (<div className="modal-container"
-            style={
-                {
-                    display:modalDisplay,
-                }
-            }>
+    return  (
+        <div className="modal-container">
             <div className="modal"> 
-                <p>Employee {props.firstName} {props.lastName} has been created</p>
-                <button className='modalBtn' onClick={onButtonClick}> Close </button>
+                <p>Employee has been created</p>
+                <button className='modalBtn'> Close </button>
             </div>
         </div>
     );
